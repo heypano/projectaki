85a4ac75f77c060e19b3ae51abccee98019770cf
Pano Papadatos <pano.papadatos@publicrelay.com>
2020-08-03 13:54:58 -0400

    initial commit with combinations

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from "react";
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap-reboot.min.css";
+import "bootstrap/dist/css/bootstrap-grid.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  combineWords,
+  destress,
+  endings,
+  endsInAVowel,
+  getCombinations,
+  getRoot
+} from "./data";
 
 function App() {
+  const [message, setMessage] = useState("");
+  const textField = useRef();
+  const isFinalVowel = endsInAVowel(message);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-12 col-md-6">
+          <div className="input-group">
+            <input type="text" ref={textField} className="form-control mr-4" />
+            <button
+              className="btn btn-primary"
+              onClick={() => setMessage(textField.current.value)}
+            >
+              Για πές
+            </button>
+          </div>
+        </div>
+        <div className="col-12">
+          <div>
+            {/*<span>{destress(getRoot(message))}</span>*/}
+            {/*<br />*/}
+            {message && (
+              <div className="d-flex flex-row flex-wrap align-items-center">
+                {getCombinations(message)}
+              </div>
+            )}
+            {/*{isFinalVowel && <span>{message} ends in a vowel</span>}*/}
+            {/*{!isFinalVowel && <span>{message} does not end in a vowel</span>}*/}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,183 @@
+import combinations from "combinations";
+import React from "react";
+
+export const greekVowels = "αάεέιίυύηήοόωώ";
+export const englishVowels = "aeiouy";
+export const vowels = `${englishVowels}${greekVowels}`;
+
+/**
+ * Mapping greek stressed vowels to their unstressed counterpart
+ * @type {}
+ */
+export const vowelStressMap = {
+  ά: "α",
+  ί: "ι",
+  έ: "ε",
+  ό: "ο",
+  ή: "η",
+  υ: "υ"
+};
+
+/**
+ * Collecting greek endings
+ * @type {}
+ */
+export const endings = {
+  ούλης: {
+    en: "oulis"
+  },
+  ούλα: {
+    en: "oula"
+  },
+  ούλι: {
+    en: "ouli"
+  },
+  άκος: {
+    en: "akos"
+  },
+  άκι: {
+    en: "aki"
+  },
+  ίτσα: {
+    en: "itsa"
+  },
+  ίτσι: {
+    en: "itsi"
+  },
+  ίτα: {
+    en: "ita"
+  },
+  πίτσι: {
+    en: "pitsi"
+  },
+  ίνι: {
+    en: "ini"
+  },
+  όνι: {
+    en: "oni"
+  },
+  // μπακλαβαδάκι: {
+  //   en: "baklavadaki"
+  // },
+  μπιρμπιλόνι: {
+    en: "birbiloni"
+  },
+  ούιν: {
+    en: "ouin"
+  }
+};
+
+/**
+ * Collecting greek links
+ * @type {}
+ */
+export const links = {
+  ουλ: {
+    en: "ουλ"
+  },
+  ακ: {
+    en: "ακ"
+  },
+  ατ: {
+    en: "ατ"
+  },
+  ιν: {
+    en: "ιν"
+  },
+  ικ: {
+    en: "ικ"
+  },
+  πιτσ: {
+    en: "πιτσ"
+  },
+  κουτσ: {
+    en: "κουτσ"
+  },
+  κουλ: {
+    en: "κουλ"
+  }
+};
+
+/**
+ * Does the provided string end in a vowel
+ * @param string
+ * @return {boolean}
+ */
+export const endsInAVowel = (string = "") => {
+  const regex = new RegExp(`[${vowels}]$`, "i");
+  return regex.test(string.trim());
+};
+
+/**
+ * Does the provided string start with a vowel
+ * @param string
+ * @return {boolean}
+ */
+export const startsWithAVowel = (string = "") => {
+  const regex = new RegExp(`^[${vowels}]`, "i");
+  return regex.test(string.trim());
+};
+
+/**
+ * Return the "root" of the word (remove final vowels)
+ * @param string
+ * @return {string}
+ */
+export const getRoot = (string = "") => {
+  // At least one non vowel, otherwise return the same
+  const regex = new RegExp(`(.*)([^${vowels}])([${vowels}]*)$`, "i");
+  return string.trim().replace(regex, "$1$2");
+};
+
+/**
+ * Return the unstressed version of a greek word
+ * @param string
+ * @return {string}
+ */
+export const destress = (string = "") => {
+  Object.keys(vowelStressMap).forEach(stressedVowel => {
+    string = string.replace(stressedVowel, vowelStressMap[stressedVowel]);
+  });
+  return string;
+};
+
+/**
+ * Combine a word with its ending and some links
+ * 1 word
+ * 1 ending
+ * N links - in order
+ * @param {string} message - the original word
+ * @param {string} ending - the ending to use in this case
+ * @param {[]} [links] - the links to use, in order
+ * @return {string} - one word combining all of the above
+ */
+export const combineWords = (message = "", ending = "", links = []) => {
+  if (links && links.length) {
+    const [firstLink, ...rest] = links;
+    return combineWords(message, combineWords(firstLink, ending), rest);
+  } else {
+    const root = destress(getRoot(message.trim()));
+    if (startsWithAVowel(ending)) {
+      return root + ending;
+    } else {
+      return root + "ο" + ending;
+    }
+  }
+};
+
+/**
+ * Get a list of all the possible combinations (endings and links) for a given word
+ * @param {string} message
+ * @param {number} numLinks
+ * @return {[]}
+ */
+export const getCombinations = (message = "") => {
+  const allLinks = [[], ...combinations(Object.keys(links))];
+  return Object.keys(endings).map((ending, endingIndex) =>
+    allLinks.map((currentLinks, linkIndex) => (
+      <div className="pr-4" key={`${endingIndex}_${linkIndex}`}>
+        {combineWords(message, ending, currentLinks)}
+      </div>
+    ))
+  );
+};
