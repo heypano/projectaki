9ad33314dd8879d88d200b32b928062cb3fc464c
Pano Papadatos <pano.papadatos@publicrelay.com>
2020-08-03 13:57:33 -0400

    Updates

---
+++ precache-manifest.4461997932fd24ad8835089b4b3c57e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3f7a2ef38f4bb7a2a680418d1132c08",
+    "url": "/projectaki/index.html"
+  },
+  {
+    "revision": "c4980ee0772647c36954",
+    "url": "/projectaki/static/css/2.6c6d28cf.chunk.css"
+  },
+  {
+    "revision": "2492dbdaa042629b9bd5",
+    "url": "/projectaki/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c4980ee0772647c36954",
+    "url": "/projectaki/static/js/2.485c1bd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/projectaki/static/js/2.485c1bd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2492dbdaa042629b9bd5",
+    "url": "/projectaki/static/js/main.4b4ad29c.chunk.js"
+  },
+  {
+    "revision": "92c76e92a4d5e423c246",
+    "url": "/projectaki/static/js/runtime-main.3483eada.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/projectaki/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,14 +11,14 @@
  * See https://goo.gl/2aRDsh
  */
 
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
 importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
+  "/projectaki/precache-manifest.4461997932fd24ad8835089b4b3c57e7.js"
 );
 
-importScripts("/precache-manifest.27b5a248283c062b2df67ba310b3b3ea.js");
-
-self.addEventListener("message", event => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
 });
@@ -33,9 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(
-  workbox.precaching.getCacheKeyForURL("/index.html"),
-  {
-    blacklist: [/^\/_/, /\/[^/?]+\.[^/]+$/]
-  }
-);
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/projectaki/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
